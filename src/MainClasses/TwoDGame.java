f5a12bb73391718f32d5f5e37e227d0c2283dff3
TwanInc <twan.schoonen@home.nl>
2016-11-22 18:53:15 +0100

    added comments

--- src/MainClasses/TwoDGame.java
+++ src/MainClasses/TwoDGame.java
@@ -7,12 +7,14 @@ import javax.swing.*;
  */
 public class TwoDGame extends JFrame{
 
+    //width of the game Frame
     private static final int WIDTH = 300;
+    //height of the game Frame
     private  static final int HEIGHT = 300;
 
     public TwoDGame() {
         super("Title test!!"); //set the title of the frame
-        
+
         setSize(WIDTH+32,HEIGHT+32);
         setVisible(true); // makes the frame visible
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //Able to close the frame
