27fb71547a9484ae9a2cbb712c55dda91d6f855b
TwanInc <twan.schoonen@home.nl>
2016-11-22 17:24:45 +0100

    made message

--- src/MainClasses/Main.java
+++ src/MainClasses/Main.java
@@ -1,9 +1,11 @@
+package MainClasses;
+
 /**
  * Created by twan on 20-11-2016.
  */
 public class Main {
 
     public static void main(String [] args) {
-        System.out.print("Hello World! *2");
+        new TwoDGame();
     }
 }
